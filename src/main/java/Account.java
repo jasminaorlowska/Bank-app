502786f3c72c36aa3a3df2fe190a481aab757566
jasminaorlowska <jorlowska3@st.swps.edu.pl>
2022-06-17 12:05:17 +0200

    Developing Main

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,17 +1,40 @@
 import java.util.Random;
 
-public class Account {
+abstract class Account {
 
     private int number;
     private double balance;
-    private Transaction[] history;
 
     public Account(double balance){
         this.balance = balance;
         Random rnd = new Random();
         int number = rnd.nextInt(999999);
         this.number = number;
-
     }
+
+    public int getNumber(){
+        return number;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public void withdraw(double amount){
+        if (amount>balance){
+            System.out.println("You don't have enough cash");
+        } else{
+        balance =- balance - amount;}
+    };
+
+    public void deposit(double amount){
+        balance =+ balance+amount;
+    };
+
+//    public void sendMoneyTo(int accountNumber, double amount){
+//        for (Account account : bankAccounts){
+//
+//        }
+//    };
 
 }
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,13 +1,42 @@
 public class User {
 
     private String username;
-    private String password;
+//    protected String password;
     private CheckingAccount account;
-    private SavingsAccount accountSaving;
+    private SavingsAccount savingsAccount;
+    private Transaction[] history;
 
     public User(String username, CheckingAccount account) {
         this.username = username;
         this.account = account;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean haveSavingsAccount(){
+        if (savingsAccount != null){return true;}
+        else{
+            return false;
+        }
+    }
+    public void addSavingsAccount(SavingsAccount account){
+        this.savingsAccount = account;
+    }
+
+    public SavingsAccount getSavingsAccount(){
+        return savingsAccount;
+    }
+
+    public CheckingAccount getAccount(){
+        return account;
+    }
+
+//    public int getAccNumber(){
+//        return 0;
+//    }
+
+
 
 }
